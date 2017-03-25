9d997609f8e84e3bf3aaadb1965bbc36d250800e
Rod Mcnew <rodmcnew@gmail.com>
2017-03-25 12:52:39 -0500

    add files

---
+++ syntax-highlighting-example.js
@@ -0,0 +1,27 @@
+let linkedIn = 'l';
+let gitHub = 'b';
+let on = ()=> {
+};
+
+import 'Have a great product'
+
+/**
+ * Rod McNew is a full stack web software engineer and proven manager.
+ *
+ * He specializes in JavaScript programming and agile product development.
+ */
+class NextSteps {
+    findOutMore() {
+        on(linkedIn);
+    }
+
+
+    viewHisCode() {
+        on(gitHub);
+    }
+}
+
+
+let bob = new NextSteps();
+bob.findOutMore();
+bob.viewHisCode();
