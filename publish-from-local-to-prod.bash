7d13275f4ade3710243ac37d4777ed474072a5bc
Rod McNew <rmcnew@relivinc.com>
2018-10-21 13:23:15 -0500

    add publisg file

---
+++ publish-from-local-to-prod.bash
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+git remote add prod git@github.com:rodmcnewdotcom/rodmcnewdotcom.github.io.git
+git push prod master -f
--- publish.bash
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-git remote add production git@github.com:rodmcnewdotcom/rodmcnewdotcom.github.io.git
-git push production master -f
